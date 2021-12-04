ce314c3f80a89a536902c31691f33a6c7206921d
Alexey2576 <alexeynikinitin@yandex.by>
2021-12-04 23:29:50 +0300

    add react-router-dom (v.6), rename component Routes to Content, Switch to Routes and Redirect to Navigate. Added NavLinks to component Header and connected other components which were made earlier (HW1 - HW4)

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import Header from './Header'
-import Routes from './Routes'
+import Content from './Routes'
+import {HashRouter} from "react-router-dom";
 
 function HW5() {
-    return (
-        <div>
-            {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
+   return (
+      <div>
+         {/*в gh-pages лучше работает HashRouter*/}
+         {/**/}
+         <HashRouter>
 
             <Header/>
+            {/*заменил название компоненты Routes на Content в связи с использованием новой версии "react-router-dom v6",*/}
+            {/*в котором испоользовал вместо Switch - Routes, а вместо Redirect - Navigate*/}
+            <Content/>
 
-            <Routes/>
-
-            {/*</HashRouter>*/}
-        </div>
-    )
+         </HashRouter>
+      </div>
+   )
 }
 
 export default HW5
--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Routes";
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
+   return (
+      <div>
+         <NavLink to={PATH.PRE_JUNIOR}>
+            Pre-junior
+         </NavLink>
+         <NavLink to={PATH.JUNIOR}>
+            Junior
+         </NavLink>
+         <NavLink to={PATH.JUNIOR_PLUS}>
+            Junior-plus
+         </NavLink>
 
-        </div>
-    )
+      </div>
+   )
 }
 
 export default Header
