f0cfebf097094fd671a1b0884d6f430a94249df0
Alexey2576 <alexeynikinitin@yandex.by>
2022-01-26 14:33:24 +0300

    HW11 was completed

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -2,38 +2,36 @@ import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
+
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+   const [value1, setValue1] = useState(0)
+   const [value2, setValue2] = useState(100)
+   const onChangeRange = (value: number) => setValue1(value)
+   return (
+      <div>
+         <h2>homeworks 10</h2>
 
-    return (
-        <div>
-            <hr/>
-            homeworks 11
+         {/*should work (должно работать)*/}
+         <div>
+            <span>{value1}</span>
+            <SuperRange value={value1} onChangeRange={onChangeRange}
+               // сделать так чтоб value1 изменялось
+            />
+         </div>
 
-            {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
-            </div>
+         <div>
+            <span>{value1}</span>
 
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
-            </div>
+            <span>{value2}</span>
+         </div>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
-        </div>
-    )
+         <hr/>
+         {/*для личного творчества, могу проверить*/}
+         {/*<AlternativeSuperRange/>*/}
+         {/*<AlternativeSuperDoubleRange/>*/}
+         <hr/>
+      </div>
+   )
 }
 
 export default HW11
